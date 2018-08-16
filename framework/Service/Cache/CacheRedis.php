2e444f9b90cc52094740f8ef5fe6007d03435600
beautymyth <903628963@qq.com>
2018-08-16 16:54:10 +0800

    redis
    超时重试

--- framework/Service/Cache/CacheRedis.php
+++ framework/Service/Cache/CacheRedis.php
@@ -22,14 +22,20 @@ class CacheRedis implements CacheContract {
      * @param string $strKey key
      * @return string or bool
      */
-    public function get($strKey) {
+    public function get($strKey, $blnTry = false) {
         try {
             $dateStartTime = getMicroTime();
-            $objReadHander = $this->getReadHander();
+            $objReadHander = $this->getReadHander($blnTry);
             return is_null($objReadHander) ? false : $objReadHander->get($strKey);
         } catch (Exception $e) {
+            //超时重试
+            $strMsg = $e->getMessage();
+            if ($strMsg == 'read error on connection' && $blnTry == false) {
+                return $this->get($strKey, true);
+            }
+            //日志记录
             $dateEndTime = getMicroTime();
-            $strLog = sprintf("\n commond:%s \n key:%s \n memo:%s \n startdate:%s \n enddate:%s \n connectinfo:%s \n", 'get', $strKey, $e->getMessage(), $dateStartTime, $dateEndTime, json_encode($this->arrCurConnectInfo));
+            $strLog = sprintf("\n commond:%s \n key:%s \n memo:%s \n startdate:%s \n enddate:%s \n connectinfo:%s \n", 'get', $strKey, $strMsg, $dateStartTime, $dateEndTime, json_encode($this->arrCurConnectInfo));
             Log::log($strLog, Config::get('const.Log.LOG_REDISERR'));
             return false;
         }
@@ -42,18 +48,24 @@ class CacheRedis implements CacheContract {
      * @param int $intTimeout 过期时间
      * @return bool
      */
-    public function set($strKey, $strValue, $intTimeout = 0) {
+    public function set($strKey, $strValue, $intTimeout = 0, $blnTry = false) {
         try {
             $dateStartTime = getMicroTime();
-            $objWriteHander = $this->getWriteHander();
+            $objWriteHander = $this->getWriteHander($blnTry);
             if ($intTimeout > 0) {
                 return is_null($objWriteHander) ? false : $objWriteHander->set($strKey, $strValue, $intTimeout);
             } else {
                 return is_null($objWriteHander) ? false : $objWriteHander->set($strKey, $strValue);
             }
         } catch (Exception $e) {
+            //超时重试
+            $strMsg = $e->getMessage();
+            if ($strMsg == 'read error on connection' && $blnTry == false) {
+                return $this->set($strKey, $strValue, $intTimeout, true);
+            }
+            //日志记录
             $dateEndTime = getMicroTime();
-            $strLog = sprintf("\n commond:%s \n key:%s \n value:%s \n memo:%s \n startdate:%s \n enddate:%s \n connectinfo:%s \n", 'set', $strKey, $strValue, $e->getMessage(), $dateStartTime, $dateEndTime, json_encode($this->arrCurConnectInfo));
+            $strLog = sprintf("\n commond:%s \n key:%s \n value:%s \n memo:%s \n startdate:%s \n enddate:%s \n connectinfo:%s \n", 'set', $strKey, $strValue, $strMsg, $dateStartTime, $dateEndTime, json_encode($this->arrCurConnectInfo));
             Log::log($strLog, Config::get('const.Log.LOG_REDISERR'));
             return false;
         }
@@ -64,14 +76,20 @@ class CacheRedis implements CacheContract {
      * @param string|array $mixKey key
      * @return int
      */
-    public function del($mixKey) {
+    public function del($mixKey, $blnTry = false) {
         try {
             $dateStartTime = getMicroTime();
-            $objWriteHander = $this->getWriteHander();
+            $objWriteHander = $this->getWriteHander($blnTry);
             return is_null($objWriteHander) ? 0 : $objWriteHander->unlink($mixKey);
         } catch (Exception $e) {
+            //超时重试
+            $strMsg = $e->getMessage();
+            if ($strMsg == 'read error on connection' && $blnTry == false) {
+                return $this->del($mixKey, true);
+            }
+            //日志记录
             $dateEndTime = getMicroTime();
-            $strLog = sprintf("\n commond:%s \n key:%s \n memo:%s \n startdate:%s \n enddate:%s \n connectinfo:%s \n", 'del', is_array($mixKey) ? json_encode($mixKey) : $mixKey, $e->getMessage(), $dateStartTime, $dateEndTime, json_encode($this->arrCurConnectInfo));
+            $strLog = sprintf("\n commond:%s \n key:%s \n memo:%s \n startdate:%s \n enddate:%s \n connectinfo:%s \n", 'del', is_array($mixKey) ? json_encode($mixKey) : $mixKey, $strMsg, $dateStartTime, $dateEndTime, json_encode($this->arrCurConnectInfo));
             Log::log($strLog, Config::get('const.Log.LOG_REDISERR'));
             return 0;
         }
