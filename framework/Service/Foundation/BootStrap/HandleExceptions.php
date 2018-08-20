a59614fab186aea3c347feec775f6a17329dd1e9
beautymyth <903628963@qq.com>
2018-08-20 10:44:34 +0800

    shutdown

--- framework/Service/Foundation/BootStrap/HandleExceptions.php
+++ framework/Service/Foundation/BootStrap/HandleExceptions.php
@@ -22,8 +22,8 @@ class HandleExceptions {
         set_exception_handler([$this, 'handleException']);
 
         set_error_handler([$this, 'handleError']);
-        
-        register_shutdown_function([$this, 'handleFatal']);
+
+        register_shutdown_function([$this, 'handleShutDown']);
     }
 
     /**
@@ -60,19 +60,23 @@ class HandleExceptions {
         $strLog = sprintf("\n errno:%s \n errstr:%s \n errfile:%s \n errline:%s \n", $strErrNo, $strErrStr, $strErrFile, $intErrLine);
         $this->objApp->make('log')->log($strLog, Config::get('const.Log.LOG_ERR'));
     }
-    
-    /**
-     * 自定义错误处理
-     */
-    public function handleError($strErrNo, $strErrStr, $strErrFile, $intErrLine) {
-        if (!(error_reporting() & $strErrNo)) {
-            //如果出现的错误不在定义接受的错误范围内，则转交给php自身处理
-            return false;
-        }
 
-        //日志记录
-        $strLog = sprintf("\n errno:%s \n errstr:%s \n errfile:%s \n errline:%s \n", $strErrNo, $strErrStr, $strErrFile, $intErrLine);
-        $this->objApp->make('log')->log($strLog, Config::get('const.Log.LOG_ERR'));
+    /**
+     * 自定义程序结束处理
+     */
+    public function handleShutDown() {
+        if (!is_null($arrError = error_get_last()) && $this->isFatal($arrError['type'])) {
+            //日志记录
+            $strLog = sprintf("\n errno:%s \n errstr:%s \n errfile:%s \n errline:%s \n", $arrError['type'], $arrError['message'], $arrError['file'], $arrError['line']);
+            $strLog = $this->objApp->make('log')->log($strLog, Config::get('const.Log.LOG_ERR'));
+        }
+    }
+
+    /**
+     * 是否为致命错误
+     */
+    protected function isFatal($strType) {
+        return in_array($strType, [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_USER_ERROR, E_PARSE]);
     }
 
     /**
