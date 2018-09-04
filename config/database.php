b9331fbc2bda1afba5884b38e1d13a8b55ec3b35
beautymyth <903628963@qq.com>
2018-09-04 13:24:50 +0800

    db businessinfo
    db businessinfo

--- config/database.php
+++ config/database.php
@@ -6,66 +6,77 @@
 return [
     /**
      * 默认业务类型
+     * 当通过表确定不了业务类型时
      */
     'default_business_type' => 'devmanager',
     /**
-     * 业务类别
+     * 业务信息
      */
-    'devmanager' => [
+    'business_info' => [
         /**
-         * 主库是否参与读
+         * 业务类别
          */
-        'master_read' => 1,
-        /**
-         * 连接超时时间
-         */
-        'connect_timeout' => 3,
-        /**
-         * 长连接
-         */
-        'persistent' => false,
-        /**
-         * 主库
-         */
-        'master' => [
-            'business' => 'devmanager',
-            'type' => 'mysql',
-            'host' => '10.100.2.235',
-            'port' => '3306',
-            'db' => 'devmanager',
-            'username' => 'appuser',
-            'password' => 'h86oFd19Ley8//NSWK/UvCzirw0cg6TolMo+RrYl0g08Pbf+4WYt1j3Mv2D8Y7ZkI/DfSg6XhI5+XkBgb4etcA=='
-        ],
-        /**
-         * 从库
-         */
-        'slave' => [
-            [
+        'devmanager' => [
+            /**
+             * 主库是否参与读
+             */
+            'master_read' => 1,
+            /**
+             * 连接超时时间
+             */
+            'connect_timeout' => 3,
+            /**
+             * 长连接
+             */
+            'persistent' => true,
+            /**
+             * 主库
+             */
+            'master' => [
                 'business' => 'devmanager',
                 'type' => 'mysql',
-                'host' => '10.100.2.235',
+                'host' => '127.0.0.1',
                 'port' => '3306',
                 'db' => 'devmanager',
-                'username' => 'appuser',
-                'password' => 'h86oFd19Ley8//NSWK/UvCzirw0cg6TolMo+RrYl0g08Pbf+4WYt1j3Mv2D8Y7ZkI/DfSg6XhI5+XkBgb4etcA=='
+                'username' => 'test',
+                'password' => '+foUvu6l60YHoUj79pj33oJLw3AUf7ix/UtvKF7DHMaO57Oq+TA+AtTrbjPf57rRUl0wurmAgwVSDPCKvpQ8Kw=='
+            ],
+            /**
+             * 从库
+             */
+            'slave' => [
+                [
+                    'business' => 'devmanager',
+                    'type' => 'mysql',
+                    'host' => '127.0.0.1',
+                    'port' => '3306',
+                    'db' => 'devmanager',
+                    'username' => 'test',
+                    'password' => '+foUvu6l60YHoUj79pj33oJLw3AUf7ix/UtvKF7DHMaO57Oq+TA+AtTrbjPf57rRUl0wurmAgwVSDPCKvpQ8Kw=='
+                ]
+            ],
+            /**
+             * 连接池信息
+             */
+            'connect_pool' => [
+                'host' => '127.0.0.1',
+                'port' => '9602',
+                /**
+                 * 连接池队列，公用连接
+                 */
+                'worker_num' => 4,
+                /**
+                 * 连接次可用连接
+                 */
+                'task_num' => 2,
+                'log_file' => 'devmanager_connect_pool.log'
             ]
-        ],
-        /**
-         * 连接池信息
-         */
-        'connect_pool' => [
-            'host' => '127.0.0.1',
-            'port' => '9602',
-            /**
-             * 连接池队列，公用连接
-             */
-            'worker_num' => 4,
-            /**
-             * 连接次可用连接
-             */
-            'task_num' => 2,
-            'log_file' => 'devmanager_connect_pool.log'
         ]
+    ],
+    /**
+     * 表信息
+     */
+    'table_info' => [
     ]
 ];
 
