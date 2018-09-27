e26b1eb8ecfa6735d73b569bd4b957580473e391
beautymyth <903628963@qq.com>
2018-09-27 13:49:02 +0800

    redis
    redis connect

--- framework/Service/Cache/RedisConnect.php
+++ framework/Service/Cache/RedisConnect.php
@@ -88,21 +88,26 @@ trait RedisConnect {
     protected function setRedisHanderWrite() {
         //1.遍历redis服务器
         foreach ($this->getRedisServer() as $arrServer) {
-            $objTmpHander = new Redis();
-            $blnFlag = $objTmpHander->pconnect($arrServer['host'], $arrServer['port'], Config::get('redis.connect_timeout'), Config::get('redis.persistent_id'));
-            if ($blnFlag) {
-                $objTmpHanderinfo = $objTmpHander->info('replication');
-                if (strtolower($objTmpHanderinfo['role']) == 'master') {
-                    $objTmpHander->setOption(Redis::OPT_READ_TIMEOUT, Config::get('redis.read_timeout'));
-                    $this->arrCurConnectInfo['write'] = $arrServer;
+            try {
+                $objTmpHander = new Redis();
+                $blnFlag = $objTmpHander->pconnect($arrServer['host'], $arrServer['port'], Config::get('redis.connect_timeout'), Config::get('redis.persistent_id'));
+                if ($blnFlag) {
+                    $objTmpHanderinfo = $objTmpHander->info('replication');
+                    if (strtolower($objTmpHanderinfo['role']) == 'master') {
+                        $objTmpHander->setOption(Redis::OPT_READ_TIMEOUT, Config::get('redis.read_timeout'));
+                        $this->arrCurConnectInfo['write'] = $arrServer;
 
-                    $this->objWriteHander = $objTmpHander;
-                    $this->arrWriteHanderInfo = $objTmpHanderinfo;
-                    $this->arrWriteHanderInfo['session_path'] = sprintf('tcp://%s:%s', $arrServer['host'], $arrServer['port']);
-                    break;
+                        $this->objWriteHander = $objTmpHander;
+                        $this->arrWriteHanderInfo = $objTmpHanderinfo;
+                        $this->arrWriteHanderInfo['session_path'] = sprintf('tcp://%s:%s', $arrServer['host'], $arrServer['port']);
+                        break;
+                    }
+                } else {
+                    throw new Exception('pconnect false');
                 }
-            } else {
-                Log::log('redis-write连接失败：' . implode(',', $arrServer), Config::get('const.Log.LOG_REDISERR'));
+            } catch (Exception $e) {
+                $strLog = sprintf('redis-write连接失败:%s,error:%s', implode(',', $arrServer), $e->getMessage());
+                Log::log($strLog, Config::get('const.Log.LOG_REDISERR'));
             }
         }
         //2.检查是否能连接到写服务器
@@ -122,21 +127,27 @@ trait RedisConnect {
         $objTmpHander = new Redis();
         $arrRedisServer = $this->getRedisServer();
         while ($intTryCount <= $intTotalCount) {
-            $strRandomKey = rand(0, count($arrRedisServer) - 1);
-            $strRandomKey = array_keys($arrRedisServer)[$strRandomKey];
-            $arrServer = $arrRedisServer[$strRandomKey];
-            $blnFlag = $objTmpHander->pconnect($arrServer['host'], $arrServer['port'], Config::get('redis.connect_timeout'), Config::get('redis.persistent_id'));
-            if ($blnFlag) {
-                $objTmpHander->setOption(Redis::OPT_READ_TIMEOUT, Config::get('redis.read_timeout'));
-                $this->arrCurConnectInfo['read'] = $arrServer;
+            try {
+                $strRandomKey = rand(0, count($arrRedisServer) - 1);
+                $strRandomKey = array_keys($arrRedisServer)[$strRandomKey];
+                $arrServer = $arrRedisServer[$strRandomKey];
+                $blnFlag = $objTmpHander->pconnect($arrServer['host'], $arrServer['port'], Config::get('redis.connect_timeout'), Config::get('redis.persistent_id'));
+                if ($blnFlag) {
+                    $objTmpHander->setOption(Redis::OPT_READ_TIMEOUT, Config::get('redis.read_timeout'));
+                    $this->arrCurConnectInfo['read'] = $arrServer;
 
-                $this->objReadHander = $objTmpHander;
-                break;
-            } else {
-                unset($arrRedisServer[$strRandomKey]);
-                Log::log('redis-read连接失败：' . implode(',', $arrServer), Config::get('const.Log.LOG_REDISERR'));
+                    $this->objReadHander = $objTmpHander;
+                    break;
+                } else {
+                    unset($arrRedisServer[$strRandomKey]);
+                    throw new Exception('pconnect false');
+                }
+            } catch (Exception $e) {
+                $strLog = sprintf('redis-read连接失败:%s,error:%s', implode(',', $arrServer), $e->getMessage());
+                Log::log($strLog, Config::get('const.Log.LOG_REDISERR'));
+            } finally {
+                $intTryCount++;
             }
-            $intTryCount++;
         }
         //2.检查是否能连接到读服务器
         if (is_null($this->objReadHander)) {
