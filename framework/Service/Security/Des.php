9d4251652f1ce99a8ce0a707df02ffce1243fdb0
beautymyth <903628963@qq.com>
2018-10-26 19:07:49 +0800

    Des
    解密失败返回原值

--- framework/Service/Security/Des.php
+++ framework/Service/Security/Des.php
@@ -51,10 +51,11 @@ class Des {
     }
 
     /**
-     * 对字符串进行解密
+     * 对字符串进行解密，失败返回原值
      * @param string $strValue 需要解密的字符串
      */
     public function decrypt($strValue) {
+        $strOldValue = $strValue;
         $strValue = base64_decode($strValue);
         $intIvLen = openssl_cipher_iv_length($this->strAesCipher);
         //获取向量
@@ -65,11 +66,11 @@ class Des {
         $strCiphertext = substr($strValue, $intIvLen + $intShaLen);
         //格式验证
         if (strlen($strIv) != 16 || strlen($strHMac) != 32 || strlen($strCiphertext) <= 0) {
-            return '';
+            return $strOldValue;
         }
         $strOriginaltext = openssl_decrypt($strCiphertext, $this->strAesCipher, Config::get('des.aes_key'), OPENSSL_RAW_DATA, $strIv);
         $strCalcMac = hash_hmac('sha256', $strCiphertext, Config::get('des.aes_key'), true);
-        return hash_equals($strHMac, $strCalcMac) ? $strOriginaltext : '';
+        return hash_equals($strHMac, $strCalcMac) ? $strOriginaltext : $strOldValue;
     }
 
 }
